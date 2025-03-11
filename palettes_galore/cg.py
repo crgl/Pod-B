ab101e3ed13b34ecd9fa57e9e6eb6b86f93951ca
Charles Gleason <36055314+crgl@users.noreply.github.com>
2025-03-11 11:17:05 -0400

    Update cg.py

--- palettes_galore/cg.py
+++ palettes_galore/cg.py
@@ -5,7 +5,9 @@ def cg(v):
     '''
     A function that takes a number between 0 and 1 as input and returns a color
     '''
-    if v < 0.5:
-        return '#bbbbbb'
-    else:
-        return (0.1, 0.1, 0.1)
+    return '#deadbe'
+    # return (v, v, 0)
+    # if v < 0.5:
+    #     return '#bbbbbb'
+    # else:
+    #     return (0.1, 0.1, 0.1)
