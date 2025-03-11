019cd2bc8ea50b79b5040ad8b099ebda63414fd0
Jessica Nardulli <131474194+narduj01@users.noreply.github.com>
2025-03-11 11:25:59 -0400

    update 'magenta'

--- palettes_galore/jn.py
+++ palettes_galore/jn.py
@@ -1,5 +1,5 @@
 import colorcet as cc
 
 def jn(v):
-    #i am hoping that high values will be purple and low values will be yellow
+    #i am hoping that high values will be magenta and low values will be yellow
     return (1, 1-v, v)
