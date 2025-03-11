36d8448ce5de7b4e43f687b3ea170d69db66da13
Charles Gleason <36055314+crgl@users.noreply.github.com>
2025-03-11 11:33:18 -0400

    Add graph that integrates everyone's palettes

--- palettes_galore/cg.py
+++ palettes_galore/cg.py
@@ -1,13 +1,8 @@
-import colorcet as cc
-
-
 def cg(v):
     '''
     A function that takes a number between 0 and 1 as input and returns a color
     '''
-    return '#deadbe'
-    # return (v, v, 0)
-    # if v < 0.5:
-    #     return '#bbbbbb'
-    # else:
-    #     return (0.1, 0.1, 0.1)
+    if v < 0.5:
+        return '#bbbbbb'
+    else:
+        return (0.1, 0.1, 0.1)
--- palettes_galore/graph.py
+++ palettes_galore/graph.py
@@ -4,15 +4,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from cg import *
+from yc import *
+from bcm import *
+from cw import *
+from jn import *
 
-pal = LinearSegmentedColormap.from_list(
-    "pal", [cg(i/100) for i in range(100)]
-)
+# # pal_funcs = [cg, bcm, jn, yc, cw, nl, kds]
+# names = ['cg', 'bcm', 'jn', 'yc', 'cw', 'nl', 'kds']
+
+# pal_funcs = [cg, bcm, jn, yc, cw, nl, kds]
+# names = ['cg', 'bcm', 'jn', 'yc', 'cw', 'nl', 'kds']
+
+
+pal_funcs = [cg, bcm, jn, yc, cw]
+names = ['cg', 'bcm', 'jn', 'yc', 'cw']
+
+pals = []
+
+for pal_func in pal_funcs:
+    pal = LinearSegmentedColormap.from_list(
+        "pal", [pal_func(i/100) for i in range(100)]
+    )
+    pals.append(pal)
 
 vals = np.arange(100)
 
 df = pd.DataFrame({'x': vals, 'y': np.sin(vals / 10), 'hue': np.cos(vals / 2)})
 
 if __name__ == '__main__':
-    sns.scatterplot(data=df, x='x', y='y', hue='hue', palette=pal)
+    fig, axes = plt.subplots(len(pals) // 3 + 1, 3, sharex=True, sharey=True, layout='constrained')
+    for i, pal in enumerate(pals):
+        ax = axes[i // 3, i % 3]
+        plt.sca(ax)
+        sns.scatterplot(data=df, x='x', y='y', hue='hue', palette=pal, legend=None)
+        # sns.scatterplot(data=df, x='x', y='y', hue='hue', palette='magma', legend=None)
+        plt.xticks([])
+        plt.yticks([])
+        plt.ylabel('')
+        plt.xlabel('')
+        ax.set_title(names[i])
     plt.show()
