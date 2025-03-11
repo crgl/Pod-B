9669e628f8d4c66edb7af131a2cc3b37782d79a3
MonahB01 <107202736+MonahB01@users.noreply.github.com>
2025-03-11 11:50:09 -0400

    make it brighter :)

--- palettes_galore/bcm.py
+++ palettes_galore/bcm.py
@@ -2,14 +2,21 @@
 import colorcet as cc
 import random
 
+alpha_lower = 'abcdefghijklmnopqrstuvwxyz'
+alpha_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def bcm(x):
     i=0
     list=["#"]
     while i < 6:
-        list.append(random.randint(0,abs(int(str(x)[-1]))))
+        if random.randint(0,x) < (x/8)*2:
+            list.append(alpha_lower[random.randint(0,25)])
+        elif random.randint(0,x) > ((x/8)*6):
+            list.append(alpha_upper[random.randint(0,25)])
+        else:
+            list.append(random.randint(0,abs(int(str(x)[-1]))))
         i+=1
     
     return("".join(str(x) for x in list))
 
-print(bcm(2))
 # %%
